75f5b8648e9dcbe7ca3a2ea78ef2baf3d73db769
Marat <94042173+Crescent17@users.noreply.github.com>
2023-04-28 16:52:57 +0300

    add get, put, patch, delete methods for specific articles with exception handling

--- app.js
+++ app.js
@@ -22,24 +22,86 @@ const Article = mongoose.model("Article", articleSchema)
 
 app.route("/articles")
     .get((req, res) => {
-    Article.find().then(articles => {
-        res.send(articles)
+        Article.find().then(articles => {
+            res.status(200).send(articles)
+        }).catch(err => {
+            res.status(401).send(err.errors.title.message)
+        })
     })
-})
     .post((req, res) => {
-    const article = new Article({
-        title: req.body.title,
-        content: req.body.content
+        const article = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        article.save().then(() => {
+            res.status(201).send(article)
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        });
     })
-    article.save().then(() => {
-        res.status(201).send(article)
-    }).catch(err => {
-        res.status(400).send(err.errors.title.message)});
-})
     .delete((req, res) => {
-    Article.deleteMany({}).then(() => {
-        res.send("Deleted!")
-    }).catch(err => {
-        res.send(err)
+        Article.deleteMany({}).then(() => {
+            res.status(200).send("Deleted!")
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        })
     })
-})
+
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({
+            title: req.params.articleTitle
+        }).then(result => {
+            if (result) {
+                res.send(result)
+            } else {
+                res.send(`There is no ${req.params.articleTitle} article`)
+            }
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        })
+    })
+    .put((req, res) => {
+        Article.updateOne({
+                title: req.params.articleTitle
+            },
+            {
+                title: req.body.title,
+                content: req.body.content
+            }).then(result => {
+            if (result.matchedCount !== 0) {
+                res.status(200).send(result)
+            } else {
+                res.send(`There is no ${req.params.articleTitle} article`)
+            }
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        })
+    })
+    .patch((req, res) => {
+        Article.updateOne({
+                title: req.params.articleTitle
+            },
+            {$set: req.body}).then(result => {
+            if (result.matchedCount !== 0) {
+                res.status(200).send(result)
+            } else {
+                res.send(`There is no ${req.params.articleTitle} article`)
+            }
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        })
+    })
+    .delete((req, res) => {
+        Article.deleteOne({
+            title: req.body.articleTitle
+        }).then(result => {
+            if (result.deletedCount !== 0) {
+                res.status(200).send('Deleted!')
+            } else {
+                res.send(`There is no ${req.params.articleTitle} article`)
+            }
+        }).catch(err => {
+            res.status(400).send(err.errors.title.message)
+        })
+    })

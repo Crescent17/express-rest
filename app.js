3126f4fdb391869dc188acae98c94d8dfdf3a232
Marat <94042173+Crescent17@users.noreply.github.com>
2023-04-28 15:10:30 +0300

    add custom validation error messages, add get, post and delete routing, in case of error send it back to client

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser");
 
 app.listen(3000)
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB')
 
 const articleSchema = mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: [true, 'Title field is required!']
     },
     content: {
         type: String,
-        required: true
+        required: [true, 'Content field is required!']
     }
 })
 const Article = mongoose.model("Article", articleSchema)
+
+app.get("/articles", (req, res) => {
+    Article.find().then(articles => {
+        res.send(articles)
+    })
+})
+
+app.post("/articles", (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        content: req.body.content
+    })
+    article.save().then(response => {
+        res.status(201).send(article)
+    }).catch(err => {
+        res.status(400).send(err.errors.title.message)});
+})
+
+app.delete("/articles", (req, res) => {
+    Article.deleteMany({}).then(() => {
+        res.send("Deleted!")
+    }).catch(err => {
+        res.send(err)
+    })
+})

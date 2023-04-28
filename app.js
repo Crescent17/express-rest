2ca641fc284e4b89ed5e2686cdded3ded59b6c85
Marat <94042173+Crescent17@users.noreply.github.com>
2023-04-28 12:46:42 +0300

    add bodyparser settings

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser");
 
 app.listen(3000)
+app.use(bodyParser.urlencoded({ extended: false }))
 mongoose.connect('mongodb://127.0.0.1:27017/wikiDB')
 
 const articleSchema = mongoose.Schema({

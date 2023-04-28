7577d0b21e0ce3e8fbb1452940ae6b77dff670a7
Marat <94042173+Crescent17@users.noreply.github.com>
2023-04-28 12:43:01 +0300

    add db connection and article schema

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express()
+const mongoose = require("mongoose")
 
 app.listen(3000)
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB')
+
+const articleSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+})
